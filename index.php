5154dd86cce0a1513c2c8006e2171ed7d588a9dd
DamianoMura <mura.damiano14@gmail.com>
2025-11-03 12:14:45 +0100

    [upd] scraper only works if the last entry has the field "cover_album_url" === ""

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-include 'scrapeCoverAlbum.php';
+
+
 $albums = json_decode(file_get_contents('library.json'), true);
 
-
-
+if ($albums[count($albums) - 1]["cover_album_url"] == "") {
+  include './scrapeCoverAlbum.php';
+  $albums = json_decode(file_get_contents('library.json'), true);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,25 +21,26 @@ $albums = json_decode(file_get_contents('library.json'), true);
 <body>
   <header>
     <h1 class="text-center my-4">JDW MUSIC</h1>
+
+  </header>
+  <main>
     <div class="container">
-      <div class="row row-cols-4 g-4">
+      <div class="row">
         <?php foreach ($albums as $album) : ?>
-          <div class="col text-center">
-            <div class="card h-100">
-              <img src="<?php echo $album['cover_album_url'] ?>" class="card-img-top" alt="<?php echo $album['title'] ?>">
-              <div class="card-body">
-                <h5 class="card-title"><?php echo $album['title'] ?></h5>
-                <p class="card-text"><?php echo $album['artist'] ?></p>
-                <p class="card-text"><?php echo $album['publish_year'] ?></p>
-              </div>
+          <div class="col-12 text-center mb-4">
+            <div class="d-flex align-items-center">
+              <img src=<?php echo $album["cover_album_url"] ?> alt=<?php echo $album["title"] ?> class="img-fluid me-3">
+              <h3><?php echo $album["artist"] ?></h3>
+              <span>&nbsp;-&nbsp;</span>
+              <h4><?php echo $album["title"] ?></h4>
+              <h5>(<?php echo $album["publish_year"] ?>)</h5>
             </div>
           </div>
         <?php endforeach; ?>
       </div>
     </div>
 
-  </header>
-
+  </main>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
